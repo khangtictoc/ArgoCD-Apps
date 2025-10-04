b10204e4ac7f9a75f7f32b63e1d188b9da7999fc
Khang Hoang Tran <tranhoangkhang09112001@gmail.com>
2025-10-04 23:57:25 +0700

    feat - test: cert-manager

--- script/create_namespace.sh
+++ script/create_namespace.sh
@@ -3,3 +3,4 @@
 echo "Create list of namespaces used only for deploying Apps"
 kubectl create namespace nginx-ingress --dry-run=client -o yaml | kubectl apply -f -
 kubectl create namespace jenkins --dry-run=client -o yaml | kubectl apply -f -
+kubectl create namespace cert-manager --dry-run=client -o yaml | kubectl apply -f -
